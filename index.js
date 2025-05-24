82be996680c73ea7a22236ef8c8c35c7300daf05
Rohan Verma <rv0181998@gmail.com>
2025-05-24 12:53:11 +0530

    notes

---
+++ index.js
@@ -0,0 +1,240 @@
+// --------------Bubble Sort-----------
+
+var nums = [5, 2, 3, 1];
+function BubbleSort(nums) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = 0; j < nums.length - i - 1; j++) {
+      if (nums[j] > nums[j + 1]) {
+        let temp = nums[j];
+        nums[j] = nums[j + 1];
+        nums[j + 1] = temp;
+      }
+    }
+  }
+  return nums;
+}
+
+console.log(BubbleSort(nums));
+
+// --------------Selection Sort-----------
+var nums = [5, 2, 3, 1];
+function SelectionSort(nums) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[minIndex] > nums[j]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      let temp = nums[i];
+      nums[i] = nums[minIndex];
+      nums[minIndex] = temp;
+    }
+  }
+  return nums;
+}
+
+console.log(SelectionSort(nums));
+
+// --------------Insertion Sort-----------
+var nums = [5, 2, 3, 1];
+function InsertionSort(nums) {
+  for (let i = 1; i < nums.length; i++) {
+    let key = nums[i];
+    let j = i - 1;
+    while (j >= 0 && nums[j] > key) {
+      nums[j + 1] = nums[j];
+      j--;
+    }
+    nums[j + 1] = key;
+  }
+  return nums;
+}
+
+console.log(InsertionSort(nums));
+
+// --------------Merge Sort-----------
+
+const numbers = [38, 27, 43, 3, 9, 82, 10];
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  const middle = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, middle));
+  const right = mergeSort(arr.slice(middle));
+
+  return merge(left, right);
+}
+
+function merge(left, right) {
+  let i = 0;
+  let j = 0;
+  let result = [];
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+  return result.concat(left.slice(i)).concat(right.slice(j));
+}
+
+console.log(mergeSort(numbers));
+
+// ---------------2.Flat this Array-----------------------
+const nested = [1, [2, [3, [4, 5]]], 6];
+
+function flattenArray(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      result = result.concat(flattenArray(arr[i]));
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+console.log(flattenArray(nested));
+
+// -------------3.Frequency of Element--------------
+// const arr = [1, 2, 2, 3, 4, 4, 4, 'a', 'a', true, true, false];
+
+function Frequency(arr) {
+  let element = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (element[arr[i]]) {
+      element[arr[i]]++;
+    } else {
+      element[arr[i]] = 1;
+    }
+  }
+  return element;
+}
+console.log(Frequency(arr));
+
+// -----------4.Count Distinct element--------------
+// const arr = [1, 2, 2, 3, 4, 4, 4, 'a', 'a', true, false, true];
+
+function CountDistinct(arr) {
+  let element = {};
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (!element[arr[i]]) {
+      element[arr[i]] = true;
+      count++;
+    }
+  }
+  return count;
+}
+console.log(CountDistinct(arr));
+
+// -------------5.Repeating element----------------
+// const arr = [1, 2, 2, 3, 4, 4, 4, 'a', 'a', true, false, true];
+
+function Repeating(arr) {
+  let element = {};
+  let repeating = [];
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (element[arr[i]]) {
+      element[arr[i]]++;
+    } else {
+      element[arr[i]] = 1;
+    }
+  }
+
+  for (let key in element) {
+    if (element[key] > 1) {
+      repeating.push(key);
+    }
+  }
+  return repeating;
+}
+console.log(Repeating(arr));
+
+// --------------6.Non-Repeating element----------------
+// const arr = [1, 2, 2, 3, 4, 4, 4, 'a', 'a', true, false, true];
+
+function NonRepeating(arr) {
+  let element = {};
+  let nonRepeating = [];
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (element[arr[i]]) {
+      element[arr[i]]++;
+    } else {
+      element[arr[i]] = 1;
+    }
+  }
+
+  for (let key in element) {
+    if (element[key] === 1) {
+      nonRepeating.push(key);
+    }
+  }
+  return nonRepeating;
+}
+console.log(NonRepeating(arr));
+
+// --------------7.Non-Repeating element in String----------------
+const str = "programming";
+
+function NonRepeating(str) {
+  let element = {};
+  let nonRepeating = [];
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (element[str[i]]) {
+      element[str[i]]++;
+    } else {
+      element[str[i]] = 1;
+    }
+  }
+
+  for (let char in element) {
+    if (element[char] === 1) {
+      nonRepeating.push(char);
+    }
+  }
+  return nonRepeating;
+}
+console.log(NonRepeating(str));
+
+// -----------8.Disjoint or Not---------------
+const a = [1, 2, 3];
+const b = [4, 5, 6];
+const c = [3, 7, 8];
+
+function Disjoint(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+console.log(Disjoint(a, c));
+
+// -----------------9.Construct an object from a string-------------------
+
+var path = "a.b.c.d.e";
+
+var key = path.split(".");
+
+var obj = {};
+var current = obj;
+
+for (let i = 0; i < key.length; i++) {
+  current[key[i]] = i === key.length - 1 ? "" : {};
+  current = current[key[i]];
+}
+console.log(obj);
